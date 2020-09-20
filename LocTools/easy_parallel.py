39db62024b81c0fac190b28ac314ae820c5be22e
Todd <todd_stan@163.com>
2020-09-20 14:59:18 +0800

    terminate all subprocess once exception is encountered

--- LocTools/easy_parallel.py
+++ LocTools/easy_parallel.py
@@ -1,22 +1,31 @@
+import os
+import subprocess
+from subprocess import PIPE
 from multiprocessing import Queue, Process, Manager
 from BasicTools.ProcessBarMulti import ProcessBarMulti
 
 
-def worker(func, tasks_queue, outputs_dict, pb):
+def worker(func, tasks_queue, outputs_dict, pb, worker_params, pid_father):
     while True:
         task = tasks_queue.get()
         if task is None:
             return
         else:
             task_id = task[0]
-            result = func(*task[1:])
+            try:
+                result = func(*task[1:], *worker_params)
+            except Exception as e:
+                print(e)
+                print('kill by itself because of exception')
+                subprocess.Popen(['kill', f'{pid_father}'], stdout=PIPE, stderr=PIPE)
+                return None
             outputs_dict[task_id] = result
-            
             if pb is not None:
                 pb.update()
+    return None
 
 
-def easy_parallel(func, tasks, n_worker=4, show_process=False):
+def easy_parallel(func, tasks, n_worker=4, show_process=False, worker_params=None):
     """
     Args:
         func: function to be called in parallel
@@ -35,8 +44,14 @@ def easy_parallel(func, tasks, n_worker=4, show_process=False):
     else:
         pb = None
 
+    if worker_params is None:
+        worker_params = [[] for worker_i in range(n_worker)]
+
+    cur_pid = os.getpid()
     for worker_i in range(n_worker):
-        thread = Process(target=worker, args=(func, tasks_queue, outputs_dict, pb))
+        thread = Process(
+                target=worker, 
+                args=(func, tasks_queue, outputs_dict, pb, worker_params[worker_i], cur_pid))
         thread.start()
         threads.append(thread)
     [thread.join() for thread in threads]
